2bc968652144e67c21201de7b2958f3d54cb55bb
Nick Kao <kobenkao@gmail.com>
2021-11-15 15:26:09 -0800

    remove duplicates

--- sql/operationalDB/proposals/sqlPWA.js
+++ sql/operationalDB/proposals/sqlPWA.js
@@ -11,10 +11,6 @@ text_classification (new)
 const article_metadata = {
   id: "<INT>",
   publish_date: "<STRING",
-    // duplicates
-  article_title: "<STRING>",
-  article_url: "<STRING>",
-    // duplicates
   category: "<STRING>", // used on UI
   image_url: "<STRING>",
   text_classification_id: "<INT>",
