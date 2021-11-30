259f501876858ae3efce0d03e4eefd61a9b2dd81
Varun Sharma <varun_sharma@live.co.uk>
2021-11-30 09:31:24 -0800

    play app link update

--- sql/operationalDB/proposals/narrationWidget.js
+++ sql/operationalDB/proposals/narrationWidget.js
@@ -3,11 +3,14 @@ Purpose of this file is to illustrate some additional table updates for the narr
 
 Proposed Tables Updates:
 
-article_metadata (new)
+widget_themes (existing)
+widget_play_app_link_included_org (new)
 
  **/
 
 const widget_themes = {
+  // It's really widget_settings but keeping current name
+
   id: "<INT>", // SERIAL PRIMARY KEY NOT NULL, - Existing
   organisation_id: "<INT>", //INTEGER REFERENCES organisations(id), - Existing
   color: "<STRING>", //VARCHAR(10), - Existing
@@ -19,21 +22,14 @@ const widget_themes = {
   border_radius: "<INT>", // - Existing
   title: "<BOOLEAN>", //DEFAULT false, - Existing
   image: "<STRING>", // DEFAULT '', - Existing
-  label: "<STRING>", // - Existing
+  label: "<STRING>", // This overwrites the default on the widget - Existing
   font_family: "<STRING>", // - Existing
   font_colour: "<STRING>", // - Existing
   font_size: "<STRING>", // - Existing
   font_weight: "<STRING>", // - Existing
   logo_colour: "<STRING>", // - Existing
-  play_app_link_text: "<STRING>", //  DEFAULT '' - Proposed
-  play_app_link: "<STRING>", //  DEFAULT ''  - Proposed
-};
 
-// OR
-
-const widget_play_app_link_included_org = {
-  id: "<INT>",
-  organisation_id: "<INT>", // unique constraint
-  play_app_link_text: "<STRING>", //  DEFAULT ''
-  play_app_link: "<STRING>", //  DEFAULT ''
+  play_app_link_included: "<boolean>", //  DEFAULT false (post launch default is true)  - Proposed
+  play_app_link_text: "<STRING>", //  DEFAULT null// VARCHAR(30) - Proposed
+  play_app_link: "<STRING>", //  DEFAULT null  - Proposed
 };
